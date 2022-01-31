d07520649c23298e84e4bfb9e86fb9fec71cfd5c
chaedol2 <chs891117@gmail.com>
2022-01-31 16:09:50 +0900

    Initial commit

---
+++ myhome/src/main/java/com/chs/myhome/controller/UserApiController.java
@@ -0,0 +1,90 @@
+package com.chs.myhome.controller;
+
+import com.chs.myhome.model.Board;
+import com.chs.myhome.model.QUser;
+import com.chs.myhome.model.User;
+import com.chs.myhome.repository.UserRepository;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
+class UserApiController {
+
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping("/users")
+    Iterable<User> all(@RequestParam(required = false) String method, @RequestParam(required = false) String text) {
+        Iterable<User> users = null;
+        if ("query".equals(method)) {
+            users = repository.findByUsernameQuery(text);
+        } else if("nativeQuery".equals(method)){
+            users = repository.findByUsernameNativeQuery(text);
+        } else if("querydsl".equals(method)){
+            QUser user = QUser.user;
+//            BooleanExpression b = user.username.contains(text);
+//            if(true){
+//                b = b.and(user.username.eq("HI"));
+//            }
+            Predicate predicate = user.username.contains(text);
+            users = repository.findAll(predicate);
+        } else if ("querydslCustom".equals(method)){
+            users = repository.findByUsernameCustom(text);
+        } else if("jdbc".equals(method)){
+            users = repository.findByUsernameJdbc(text);
+        } else {
+            users = repository.findAll();
+        }
+//       log.debug("getBoards().size() 호출전");
+//       log.debug("getBoards().size() : {}", users.get(0).getBoards().size());
+//       log.debug("getBoards().size() 호출후");
+        return users;
+    }
+
+    @PostMapping("/users")
+    User newUser(@RequestBody User newUser) {
+        return repository.save(newUser);
+    }
+
+    // Single item
+
+    @GetMapping("/users/{id}")
+    User one(@PathVariable Long id) {
+
+        return repository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/users/{id}")
+    User replaceUser(@RequestBody User newUser, @PathVariable Long id) {
+
+        return repository.findById(id)
+                .map(user -> {
+//                    user.setTitle(newUser.getTitle());
+//                    user.setContent(newUser.getContent());
+//                    user.setBoards(newUser.getBoards());
+                    user.getBoards().clear();
+                    user.getBoards().addAll(newUser.getBoards());
+                    for(Board board : user.getBoards()){
+                        board.setUser(user);
+                    }
+                    return repository.save(user);
+                })
+                .orElseGet(() -> {
+                    newUser.setId(id);
+                    return repository.save(newUser);
+                });
+    }
+
+    @DeleteMapping("/users/{id}")
+    void deleteUser(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
---
+++ myhome/src/main/java/com/chs/myhome/service/UserService.java
@@ -0,0 +1,28 @@
+package com.chs.myhome.service;
+
+import com.chs.myhome.model.Role;
+import com.chs.myhome.model.User;
+import com.chs.myhome.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User save(User user) {
+        String encodePassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodePassword);
+        user.setEnabled(true);
+        Role role = new Role();
+        role.setId(1l);
+        user.getRoles().add(role);
+        return userRepository.save(user);
+    }
+}
